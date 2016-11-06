ad7de8e7499a998a25980a2f210c775482610681
wangzihang008 <wangzihang008@gmail.com>
2016-11-06 17:06:34 +1300

    Fixed openweather app api code

--- running/www/scripts/index.js
+++ running/www/scripts/index.js
@@ -18,8 +18,8 @@
         var receivedElement = parentElement.querySelector('.received');
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');*/
-        $('#get-weather-btn').click(getWeatherWithZipCode());
-        //$('#test').text("test2");
+        $('#get-weather-btn').click(getWeatherWithZipCode);
+        
     };
 
     function onPause() {
