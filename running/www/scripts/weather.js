4b54d2ce945d1cfb9e382280877bfaa064cfe392
wangzihang008 <wangzihang008@gmail.com>
2016-11-06 15:59:03 +1300

    Add project files.

---
+++ running/www/scripts/weather.js
@@ -0,0 +1,49 @@
+﻿var OpenWeatherAppKey = "af5a509d5fbf88c997013127bb5ce8a8";
+
+function getWeatherWithZipCode() {
+    $('#test').text("test2");
+    var zipcode = $('#zip-code-input').val();
+    $('#error-msg').show();
+    $('#error-msg').text("Error retrieving data. ");
+
+    var queryString =
+      'http://api.openweathermap.org/data/2.5/weather?zip='
+      + zipcode + ',us&appid=' + OpenWeatherAppKey;
+
+    $.getJSON(queryString, function (results) {
+
+        showWeatherData(results);
+        
+    }).fail(function (jqXHR) {
+        $('#error-msg').show();
+        $('#error-msg').text("Error retrieving data. " + jqXHR.statusText);
+    });
+
+    return false;
+}
+
+function showWeatherData(results) {
+
+    if (results.weather.length) {
+
+        $('#error-msg').hide();
+        $('#weather-data').show();
+
+        $('#title').text(results.name);
+        $('#temperature').text(results.main.temp);
+        $('#wind').text(results.wind.speed);
+        $('#humidity').text(results.main.humidity);
+        $('#visibility').text(results.visibility);
+
+        var sunriseDate = new Date(results.sys.sunrise * 1000);
+        $('#sunrise').text(sunriseDate.toLocaleTimeString());
+
+        var sunsetDate = new Date(results.sys.sunset * 1000);
+        $('#sunset').text(sunsetDate.toLocaleTimeString());
+
+    } else {
+        $('#weather-data').hide();
+        $('#error-msg').show();
+        $('#error-msg').text("Error retrieving data. ");
+    }
+}
